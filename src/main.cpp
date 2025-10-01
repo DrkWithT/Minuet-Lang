6932480e58a4e2cbcfa0a0edf76e3e52d9ca2fee
drkwitht <derektan3510@gmail.com>
2025-09-30 17:33:30 -0700

    Integrated dud driver to main.

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <print>
+
+#include "driver/driver.hpp"
+
+constexpr auto minuet_version_major = 0;
+constexpr auto minuet_version_minor = 0;
+constexpr auto minuet_version_patch = 1;
+
+using namespace Minuet;
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::println(std::cerr, "Usage: ./minuetm [info | <program-file>]\n\tinfo []: shows usage info and version.");
+        return 1;
+    }
+
+    std::string arg_1 {argv[1]};
+
+    if (arg_1 == "info") {
+        std::println("minuetm v{}.{}.{}\n\nUsage: ./minuetm [info | <program-file>]\n\tinfo []: shows usage info and version.", minuet_version_major, minuet_version_minor, minuet_version_patch);
+        return 0;
+    }
+
+    Driver::Compilation::CompileDriver compiler;
+    const auto parse_ok = compiler.parse_sources({argv[1]});
+
+    return (parse_ok) ? 0 : 1 ;
+}
