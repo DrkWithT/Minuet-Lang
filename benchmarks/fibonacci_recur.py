d7e460045bc23922864771862c6376ca2f971ec9
drkwitht <derektan3510@gmail.com>
2025-10-08 12:54:50 -0700

    Added Python 3.12 fibonacci prgm for comparison.

---
+++ benchmarks/fibonacci_recur.py
@@ -0,0 +1,28 @@
+"""
+    fib.py\n
+    This is for a running time comparison of `fibonacci.mnl` upon my language's VM vs. Python 3's VM.\n
+    By: DrkWithT\n
+"""
+
+import time
+
+def fib(n):
+    """
+        This uses the naive recursive method for finding the n-th Fibonacci term like the corresponding Minuet program.\n
+    """
+    if n < 2:
+        return n
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+if __name__ == '__main__':
+    pre_run_time = time.process_time_ns()
+    answer = fib(35)
+    running_time = time.process_time_ns() - pre_run_time
+
+    print(f'\x1b[1;33mFinished in {running_time / 1000000}ms\x1b[0m')
+
+    if answer == 9227465:
+        exit(0)
+    else:
+        exit(1)
