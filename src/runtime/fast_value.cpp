a957124bf3573c1246e33c896d0628f098749234
drkwitht <derektan3510@gmail.com>
2025-10-11 16:17:36 -0700

    Replaced slower Value variant.

---
+++ src/runtime/fast_value.cpp
@@ -0,0 +1,359 @@
+#include <format>
+#include "runtime/fast_value.hpp"
+
+namespace Minuet::Runtime {
+    auto FastValue::negate() & -> bool {
+        switch (tag()) {
+        case FVTag::boolean:
+            m_data.scalar_v ^= 0b1;
+            return true;
+        default:
+            return false;
+        }
+    }
+
+    [[nodiscard]] auto FastValue::operator*(const FastValue& arg) & noexcept -> FastValue {
+        const auto self_tag = tag();
+
+        if (self_tag != arg.tag()) {
+            return {};
+        }
+
+        switch (self_tag) {
+        case FVTag::int32:
+            return m_data.scalar_v * arg.m_data.scalar_v;
+        case FVTag::flt64:
+            return m_data.dbl_v * arg.m_data.dbl_v;
+        default:
+            return {};
+        }
+    }
+
+    [[nodiscard]] auto FastValue::operator/(const FastValue& arg) & -> FastValue {
+        const auto self_tag = tag();
+
+        if (self_tag != arg.tag()) {
+            return {};
+        }
+
+        switch (self_tag) {
+        case FVTag::int32:
+            if (arg) {
+                return m_data.scalar_v / arg.m_data.scalar_v;
+            }
+
+            return {};
+        case FVTag::flt64:
+            if (arg) {
+                return m_data.dbl_v / arg.m_data.dbl_v;
+            }
+
+            return {};
+        default:
+            return {};
+        }
+    }
+
+    [[nodiscard]] auto FastValue::operator%(const FastValue& arg) & -> FastValue {
+        const auto self_tag = tag();
+
+        if (self_tag != arg.tag()) {
+            return {};
+        }
+
+        switch (self_tag) {
+        case FVTag::int32:
+            if (arg) {
+                return m_data.scalar_v % arg.m_data.scalar_v;
+            }
+
+            return {};
+        default:
+            return {};
+        }
+    }
+
+    [[nodiscard]] auto FastValue::operator+(const FastValue& arg) & noexcept -> FastValue {
+        const auto self_tag = tag();
+
+        if (self_tag != arg.tag()) {
+            return {};
+        }
+
+        switch (self_tag) {
+        case FVTag::int32:
+            return m_data.scalar_v + arg.m_data.scalar_v;
+        case FVTag::flt64:
+            return m_data.dbl_v + arg.m_data.dbl_v;
+        default:
+            return {};
+        }
+    }
+
+    [[nodiscard]] auto FastValue::operator-(const FastValue& arg) & noexcept -> FastValue {
+        const auto self_tag = tag();
+
+        if (self_tag != arg.tag()) {
+            return {};
+        }
+
+        switch (self_tag) {
+        case FVTag::int32:
+            return m_data.scalar_v - arg.m_data.scalar_v;
+        case FVTag::flt64:
+            return m_data.dbl_v - arg.m_data.dbl_v;
+        default:
+            return {};
+        }
+    }
+
+    auto FastValue::operator*=(const FastValue& arg) & noexcept -> FastValue& {
+        const auto self_tag = tag();
+
+        if (self_tag != arg.tag()) {
+            m_data.dud = 0;
+            m_tag = FVTag::dud;
+
+            return *this;
+        }
+
+        switch (self_tag) {
+        case FVTag::int32:
+            m_data.scalar_v *= arg.m_data.scalar_v;
+            break;
+        case FVTag::flt64:
+            m_data.dbl_v *= arg.m_data.dbl_v;
+            break;
+        default:
+            m_data.dud = 0;
+            m_tag = FVTag::dud;
+            break;
+        }
+
+        return *this;
+    }
+
+    auto FastValue::operator/=(const FastValue& arg) & -> FastValue& {
+        const auto self_tag = tag();
+
+        if (self_tag != arg.tag()) {
+            m_data.dud = 0;
+            m_tag = FVTag::dud;
+
+            return *this;
+        }
+
+        switch (self_tag) {
+        case FVTag::int32:
+            if (arg) {
+                m_data.scalar_v /= arg.m_data.scalar_v;
+            } else {
+                m_data.dud = 0;
+                m_tag = FVTag::dud;
+            }
+            break;
+        case FVTag::flt64:
+            if (arg) {
+                m_data.dbl_v /= arg.m_data.dbl_v;
+            } else {
+                m_data.dud = 0;
+                m_tag = FVTag::dud;
+            }
+            break;
+        default:
+            m_data.dud = 0;
+            m_tag = FVTag::dud;
+            break;
+        }
+
+        return *this;
+    }
+
+    auto FastValue::operator%=(const FastValue& arg) & -> FastValue& {
+        const auto self_tag = tag();
+
+        if (self_tag != arg.tag()) {
+            m_data.dud = 0;
+            m_tag = FVTag::dud;
+
+            return *this;
+        }
+
+        switch (self_tag) {
+        case FVTag::int32:
+            if (arg) {
+                m_data.scalar_v /= arg.m_data.scalar_v;
+            } else {
+                m_data.dud = 0;
+                m_tag = FVTag::dud;
+            }
+            break;
+        default:
+            m_data.dud = 0;
+            m_tag = FVTag::dud;
+            break;
+        }
+
+        return *this;
+    }
+
+    auto FastValue::operator+=(const FastValue& arg) & noexcept -> FastValue& {
+        const auto self_tag = tag();
+
+        if (self_tag != arg.tag()) {
+            m_data.dud = 0;
+            m_tag = FVTag::dud;
+
+            return *this;
+        }
+
+        switch (self_tag) {
+        case FVTag::int32:
+            m_data.scalar_v += arg.m_data.scalar_v;
+            break;
+        case FVTag::flt64:
+            m_data.dbl_v += arg.m_data.dbl_v;
+            break;
+        default:
+            m_data.dud = 0;
+            m_tag = FVTag::dud;
+            break;
+        }
+
+        return *this;
+    }
+
+    auto FastValue::operator-=(const FastValue& arg) & noexcept -> FastValue& {
+        const auto self_tag = tag();
+
+        if (self_tag != arg.tag()) {
+            m_data.dud = 0;
+            m_tag = FVTag::dud;
+
+            return *this;
+        }
+
+        switch (self_tag) {
+        case FVTag::int32:
+            m_data.scalar_v -= arg.m_data.scalar_v;
+            break;
+        case FVTag::flt64:
+            m_data.dbl_v -= arg.m_data.dbl_v;
+            break;
+        default:
+            m_data.dud = 0;
+            m_tag = FVTag::dud;
+            break;
+        }
+
+        return *this;
+    }
+
+    [[nodiscard]] auto FastValue::operator==(const FastValue& arg) const& -> bool {
+        const auto self_tag = tag();
+
+        if (self_tag != arg.tag()) {
+            return false;
+        }
+
+        switch (self_tag) {
+        case FVTag::int32:
+            return m_data.scalar_v == arg.m_data.scalar_v;
+        case FVTag::flt64:
+            return m_data.dbl_v == arg.m_data.dbl_v;
+        default:
+            break;
+        }
+
+        return false;
+    }
+
+    [[nodiscard]] auto FastValue::operator<(const FastValue& arg) const& -> bool {
+        const auto self_tag = tag();
+
+        if (self_tag != arg.tag()) {
+            return false;
+        }
+
+        switch (self_tag) {
+        case FVTag::int32:
+            return m_data.scalar_v < arg.m_data.scalar_v;
+        case FVTag::flt64:
+            return m_data.dbl_v < arg.m_data.dbl_v;
+        default:
+            break;
+        }
+
+        return false;
+    }
+
+    [[nodiscard]] auto FastValue::operator>(const FastValue& arg) const& -> bool {
+        const auto self_tag = tag();
+
+        if (self_tag != arg.tag()) {
+            return false;
+        }
+
+        switch (self_tag) {
+        case FVTag::int32:
+            return m_data.scalar_v > arg.m_data.scalar_v;
+        case FVTag::flt64:
+            return m_data.dbl_v > arg.m_data.dbl_v;
+        default:
+            break;
+        }
+
+        return false;
+    }
+
+    [[nodiscard]] auto FastValue::operator<=(const FastValue& arg) const& -> bool {
+        const auto self_tag = tag();
+
+        if (self_tag != arg.tag()) {
+            return false;
+        }
+
+        switch (self_tag) {
+        case FVTag::int32:
+            return m_data.scalar_v <= arg.m_data.scalar_v;
+        case FVTag::flt64:
+            return m_data.dbl_v <= arg.m_data.dbl_v;
+        default:
+            break;
+        }
+
+        return false;
+    }
+
+    [[nodiscard]] auto FastValue::operator>=(const FastValue& arg) const& -> bool {
+        const auto self_tag = tag();
+
+        if (self_tag != arg.tag()) {
+            return false;
+        }
+
+        switch (self_tag) {
+        case FVTag::int32:
+            return m_data.scalar_v >= arg.m_data.scalar_v;
+        case FVTag::flt64:
+            return m_data.dbl_v >= arg.m_data.dbl_v;
+        default:
+            break;
+        }
+
+        return false;
+    }
+
+    [[nodiscard]] auto FastValue::to_string() const& -> std::string {
+        switch (tag()) {
+        case FVTag::boolean:
+            return std::format("{}", m_data.scalar_v != 0);
+        case FVTag::int32:
+            return std::format("{}", m_data.scalar_v);
+        case FVTag::flt64:
+            return std::format("{}", m_data.dbl_v);
+        case FVTag::dud:
+            return "(dud)";
+        }
+    }
+}
---
+++ src/runtime/fast_value.hpp
@@ -0,0 +1,90 @@
+#ifndef MINUET_FAST_VALUE_HPP
+#define MINUET_FAST_VALUE_HPP
+
+#include <cstdint>
+#include <string>
+
+namespace Minuet::Runtime {
+    enum class FVTag : uint8_t {
+        dud,
+        boolean,
+        int32,
+        flt64,
+    };
+
+    class FastValue {
+    private:
+        union {
+            uint8_t dud;
+            int scalar_v;
+            double dbl_v;
+            // std::size_t ref_v;
+        } m_data;
+        FVTag m_tag;
+
+        [[nodiscard]] constexpr auto tag() const& noexcept -> FVTag {
+            return m_tag;
+        }
+
+    public:
+        constexpr FastValue() noexcept
+        : m_data {}, m_tag {FVTag::dud} {
+            m_data.dud = 0;
+        }
+
+        constexpr FastValue(bool b) noexcept
+        : m_data {}, m_tag {FVTag::boolean} {
+            m_data.scalar_v = static_cast<int>(b);
+        }
+
+        constexpr FastValue(int i) noexcept
+        : m_data {}, m_tag {FVTag::int32} {
+            m_data.scalar_v = i;
+        }
+
+        constexpr FastValue(double d) noexcept
+        : m_data {}, m_tag {FVTag::flt64} {
+            m_data.dbl_v = d;
+        }
+
+        [[nodiscard]] constexpr auto is_none() const& -> bool {
+            return m_tag == FVTag::dud;
+        }
+
+        [[nodiscard]] auto negate() & -> bool;
+
+        [[nodiscard]] explicit operator bool(this auto&& self) noexcept {
+            switch (self.tag()) {
+            case FVTag::boolean:
+            case FVTag::int32:
+                return self.m_data.scalar_v != 0;
+            case FVTag::flt64:
+                return self.m_data.dbl_v != 0.0;
+            default:
+                return false;
+            }
+        }
+
+        [[nodiscard]] auto operator*(const FastValue& arg) & noexcept -> FastValue;
+        [[nodiscard]] auto operator/(const FastValue& arg) & -> FastValue;
+        [[nodiscard]] auto operator%(const FastValue& arg) & -> FastValue;
+        [[nodiscard]] auto operator+(const FastValue& arg) & noexcept -> FastValue;
+        [[nodiscard]] auto operator-(const FastValue& arg) & noexcept -> FastValue;
+
+        auto operator*=(const FastValue& arg) & noexcept -> FastValue&;
+        auto operator/=(const FastValue& arg) & -> FastValue&;
+        auto operator%=(const FastValue& arg) & -> FastValue&;
+        auto operator+=(const FastValue& arg) & noexcept -> FastValue&;
+        auto operator-=(const FastValue& arg) & noexcept -> FastValue&;
+
+        [[nodiscard]] auto operator==(const FastValue& arg) const& -> bool;
+        [[nodiscard]] auto operator<(const FastValue& arg) const& -> bool;
+        [[nodiscard]] auto operator>(const FastValue& arg) const& -> bool;
+        [[nodiscard]] auto operator<=(const FastValue& arg) const& -> bool;
+        [[nodiscard]] auto operator>=(const FastValue& arg) const& -> bool;
+
+        [[nodiscard]] auto to_string() const& -> std::string;
+    };
+}
+
+#endif
